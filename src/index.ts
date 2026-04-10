b51babcb79cd9c54e1d4a8f869ac751c323dc039
ashok <hs4486952@gmail.com>
2026-04-10 14:01:15 +0530

    setup the project

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import type { Request, Response } from "express"
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+
+
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
